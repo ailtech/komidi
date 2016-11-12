e2af577dd4fa369e257c0918c33f72d49c1e0c6e
ailtech <lefevre451@gmail.com>
2016-11-12 22:52:20 +0400

    Page inscription provisoire en attente d'un devellopeur

--- vue/vueInscription.php
+++ vue/vueInscription.php
@@ -1,37 +1,38 @@
-<?php 
-require 'include/config.php';   
-//test
+<?php
+require 'include/config.php';
+
 //Titre onglet
 $titretab = "Komidi Inscription";
 // Menu de la page
-ob_start(); 
-foreach($menupage as $page_name => $page_url) 
+ob_start();
+foreach($menupage as $page_name => $page_url)
 {
-	$class = '';
-	if ($current_page == $page_url) {
-		$class = 'active';
-	}
-	echo "<li class='$class'>
+    $class = '';
+    if ($current_page == $page_url) {
+        $class = 'active';
+    }
+    echo "<li class='$class'>
 		<a href='$page_url'>".$page_name ."</a>".
-	 "</li>";
+        "</li>";
 }
 $menupage = ob_get_clean();
 
 //Contenu de la page
 ob_start();
 
-echo "<form action='#' method='POST'>
+echo "
+<form action='index.php?action=demandeInscription' method='POST'>
         <div class='form-group'>
             <label for='nom'>Nom:</label>
-            <input type='text' class='form-control' id='nom'>
+            <input type='text' class='form-control' id='nom' name='nom'>
         </div>
         <div class='form-group'>
             <label for='prenom'>Prenom:</label>
-            <input type='text' class='form-control' id='prenom'>
+            <input type='text' class='form-control' id='prenom' name='prenom'>
         </div>
         <div class='form-group'>
             <label for='email'>email:</label>
-            <input type='email' class='form-control' id='email'>
+            <input type='email' class='form-control' id='email' name='email'>
         </div>
         <div class='form-group'>
             <label for='c_email'>Confirmation email:</label>
@@ -39,21 +40,21 @@ echo "<form action='#' method='POST'>
         </div>
         <div class='form-group'>
             <label for='mdp'>Mot de passe:</label>
-            <input type='password' class='form-control' id='mdp'>
+            <input type='password' class='form-control' id='mdp' name='mdp'>
         </div>
         <div class='form-group'>
-            <label for='c_mdp'>Name:</label>
+            <label for='c_mdp'>Confirmation mot de passe:</label>
             <input type='password' class='form-control' id='c_md'>
         </div>
         <div class='checkbox'>
-            <label><input type='checkbox'>Homme</label>
-            <label><input type='checkbox'>Femme</label>
+            <label><input type='checkbox' name='sex' value='h'>Homme</label>
+            <label><input type='checkbox' name='sex' value='f'>Femme</label>
         </div>
-        <button type='submit' class='btn btn-default'>Submit</button>
+        <button type='submit' class='btn btn-default' name='envoyeInscription' value='inscription'>Submit</button>
     </form>";
 $contenupage = ob_get_clean();
 
 //Appel de template
 require "vue/vueSidebar.php";
 require "vue/template.tpl";
- ?>
+?>
