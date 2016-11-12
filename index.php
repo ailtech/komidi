31a426c6648b757d43bc2e2af7a1bf3df9a937bb
ailteck <lefevre451@gmail.com>
2016-11-12 12:21:41 +0100

    ajout de Robert , dispatcher

--- index.php
+++ index.php
@@ -1,4 +1,41 @@
-<?php 	
-	//require 'include/config.php t ';
-	require 'vue/vueAccueil.php';
+<?php
+
+
+session_start();
+
+require('./controleur/Controleur.php');
+//echo $_GET['action'] ;
+//exit;
+try {
+	if (isset($_GET['action'])  and !empty($_GET['action']))
+	{
+		if ($_GET['action'] == 'Accueil') {
+			accueil() ; // Acceuil du site
+			exit;
+		}
+		if ($_GET['action'] == 'login') {
+			login();  // connexion
+			exit;
+		}
+		if ($_GET['action'] == 'connec') {
+			connec() ; // Acceuil du site
+			exit;
+		}
+		if (isset($_GET['action'] )) {
+			if(!empty($_GET['action'] )) {
+				erreur() ; // Acceuil du site
+				exit;
+			}
+		}
+	}
+	else {
+
+		accueil();
+	}
+
+
+}
+catch (Exception $e) {
+	erreur($e->getMessage());
+}
 ?>
