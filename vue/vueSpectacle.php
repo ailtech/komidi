887c30356e2dcacfd158e0c7fc56eaca18678ea5
ailtech <lefevre451@gmail.com>
2016-11-13 13:46:45 +0400

    Ajout de la page vueSpectacle.php pour afficher les information relative a unseul spectacle

---
+++ vue/vueSpectacle.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexandre
+ * Date: 13/11/16
+ * Time: 13:37
+ */
+require 'include/config.php';
+
+//include './modele/modele.class.php';
+
+//$spectacles = new Spectacle($DB_cnx);
+
+//Titre onglet
+$titretab = "Komidi Accueil";
+// Menu de la page
+ob_start();
+foreach($menupage as $page_name => $page_url)
+{
+    $class = '';
+    if ($current_page == $page_url) {
+        $class = 'active';
+    }
+    echo "<li class='$class'>
+		<a href='$page_url'>".$page_name ."</a>".
+        "</li>";
+}
+$menupage = ob_get_clean();
+
+//Contenu de la page
+ob_start();
+echo "<div class='row affiches'>";
+
+if( !isset( $_GET['id'] ) or empty( $_GET['id'] ) ){
+    //erreur l'id na pas etait envoyer
+}
+else{
+    $id = $_GET['id'];
+    $spectacle = $spectacles->getSpectacle($id);
+    //creer une methode pour afficher un spectacle ou afficher directement dans la vue ici
+
+}
+
+echo "</div><!-- .row affiches -->";
+$contenupage = ob_get_clean();
+
+//Appel de template
+require "vue/vueSidebar.php";
+require "vue/template.tpl";
+?>
