2f43dbb98377b6c81052c291580f839110185817
ailtech <lefevre451@gmail.com>
2016-11-28 07:38:26 +0400

    ppe lundi 21 novembre

--- vue/vueInscription.php
+++ vue/vueInscription.php
@@ -1,6 +1,5 @@
 <?php
-require 'include/config.php';
-
+require_once 'include/config.php';
 //Titre onglet
 $titretab = "Komidi Inscription";
 // Menu de la page
@@ -12,49 +11,49 @@ foreach($menupage as $page_name => $page_url)
         $class = 'active';
     }
     echo "<li class='$class'>
-		<a href='$page_url'>".$page_name ."</a>".
+        <a href='$page_url'>".$page_name ."</a>".
         "</li>";
 }
 $menupage = ob_get_clean();
-
-//Contenu de la page
 ob_start();
+?>
+    <div class="container">
+        <form method="post" class="well">
+            <legend>Formulaire d'inscription</legend>
+            <div class="form-group">
+                <label for="nom">Nom</label>
+                <input type="text" class="form-control" id="nom" placeholder="Ex: Jean" name="nom" onblur="verifNom(this)"/>
+            </div>
 
-echo "
-<form action='index.php?action=demandeInscription' method='POST'>
-        <div class='form-group'>
-            <label for='nom'>Nom:</label>
-            <input type='text' class='form-control' id='nom' name='nom'>
-        </div>
-        <div class='form-group'>
-            <label for='prenom'>Prenom:</label>
-            <input type='text' class='form-control' id='prenom' name='prenom'>
-        </div>
-        <div class='form-group'>
-            <label for='email'>email:</label>
-            <input type='email' class='form-control' id='email' name='email'>
-        </div>
-        <div class='form-group'>
-            <label for='c_email'>Confirmation email:</label>
-            <input type='email' class='form-control' id='c_email'>
-        </div>
-        <div class='form-group'>
-            <label for='mdp'>Mot de passe:</label>
-            <input type='password' class='form-control' id='mdp' name='mdp'>
-        </div>
-        <div class='form-group'>
-            <label for='c_mdp'>Confirmation mot de passe:</label>
-            <input type='password' class='form-control' id='c_md'>
-        </div>
-        <div class='checkbox'>
-            <label><input type='checkbox' name='sex' value='h'>Homme</label>
-            <label><input type='checkbox' name='sex' value='f'>Femme</label>
-        </div>
-        <button type='submit' class='btn btn-default' name='envoyeInscription' value='inscription'>Submit</button>
-    </form>";
+            <div class="form-group">
+                <label for="prenom">Prenom</label>
+                <input type="text" class="form-control" id="prenom" placeholder="Ex: Dupon" name="prenom" onblur="verifPrenom(this)"/>
+            </div>
+
+            <div class="form-group">
+                <label for="pseudo">Pseudo</label>
+                <input type="text" class="form-control" id="pseudo" placeholder="Ex: jeanjean" name="pseudo" onblur="verifPseudo(this)"/>
+            </div>
+
+            <div class="form-group">
+                <label for="mdp">Mot de passe</label>
+                <input type="password" class="form-control" id="mdp" placeholder="Ex: 123456" name="mdp" onblur="verifPassword1(this)"/>
+            </div>
+
+            <div class="form-group">
+                <label for="mdpverif">Retapez le mot de passe</label>
+                <input type="password" class="form-control" id="mdpverif" placeholder="Ex: 123456" name="mdpverif" onblur="verifPassword2(this)"/>
+            </div>
+
+            <div class="form-group">
+                <label for="mail">Email</label>
+                <input type="text" class="form-control" id="mail" placeholder="Ex: jeanjean@gmail.com" name="mail" onblur="verifMail(this)"/>
+            </div>
+
+            <input type="submit" class="btn btn-default" value="Valider"/>
+        </form>
+    </div>
+<?php
 $contenupage = ob_get_clean();
-
-//Appel de template
-require "vue/vueSidebar.php";
-require "vue/template2.tpl";
+require 'vue/template2.tpl';
 ?>
