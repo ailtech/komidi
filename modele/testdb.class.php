05d712584536c7355c2321172fe8b346a73cb14e
ailtech <lefevre451@gmail.com>
2017-05-24 17:04:08 +0400

    ajout documentation des classe de test

--- modele/testdb.class.php
+++ modele/testdb.class.php
@@ -5,17 +5,38 @@
  * Date: 24/05/17
  * Time: 10:21
  */
+
+//importation de la class a tester
 require_once 'db.class.php';
+
+/**
+ * Class testdb TestUnitaire sur objet db
+ * @author Alexandre
+ */
 
 class testdb extends PHPUnit_Framework_TestCase{
 
+    /**
+     * Variable utiliser pour contenir la bdd.
+     * @var db PDO $db Variable qui va contenir la connexion
+     */
     public $db;
 
+    /**
+     * Le constructeur permet d'instancier l'objet sur le quel on va faire des test
+     * testdb constructeur.
+     */
     function testdb(){
+
+        //instanciation de l'objet de test
         $this->db = new db();
     }
 
+    /**
+     * Methode pour tester la connection elle doit tester la nullite
+     */
     public function testDbConnectionNotNull(){
+
         $this->assertNotNull( $this->db->getCo(),"**retour non null**" );
     }
 
--- modele/testmail.class.php
+++ modele/testmail.class.php
@@ -5,25 +5,40 @@
  * Date: 24/05/17
  * Time: 10:45
  */
+
+// class necessaire pour realiser teste unitaire
 require_once 'db.class.php';
 require_once 'mail.class.php';
 
+/**
+ * Class testmail TestUnitaire sur la class mail
+ * @author Alexandre
+ */
 class testmail extends PHPUnit_Framework_TestCase{
 
+    /**
+     * @var Mail Mail $mail déclaration variable pour contenir objet mail pour realiser les test
+     */
     public $mail;
 
+    /**
+     * testmail constructeur.
+     */
     function testmail(){
 
         //header que l'on va utiliser
         $headers  = 'MIME-Version: 1.0' . "\r\n";
         $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
         $headers .= 'From: testUnitaire testmail.class.php'. "\r\n";
-        //***************
+        //instanciation objet Mail
         $this->mail = new Mail("lefevre451@gmail.com","Contact Komidi","testPhpUnit",$headers);
 
 
     }
 
+    /**
+     * Methode de testEnvoyeMail
+     */
     public function testMailEnvoye(){
         $this->assertEquals($this->mail->envoyeMail( "Monsieur", "TestUnitaire", "TestUnitaire","Saint-Louis", "97450", "lefevre451@gmail.com" ),true);
     }
